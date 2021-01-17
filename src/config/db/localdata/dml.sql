dab6bf67559bb0903dc35217aa659c631d149ef7
yuto-ohta <yutohta@gmail.com>
2021-01-17 12:52:17 +0900

    chore: 初期データのパスワードハッシュを設定

--- src/config/db/localdata/dml.sql
+++ src/config/db/localdata/dml.sql
@@ -1,11 +1,12 @@
+-- 初期データのパスワードは"Test1111"
 INSERT INTO `users` (`name`, `password`) VALUES
-('まるお', 'password'),
-('トマト君', 'password'),
-('マスク侍', 'password'),
-('腕時計両腕ちゃん', 'password'),
-('わたしの中の悪魔', 'password'),
-('眠り姫', 'password'),
-('べらぼう太郎', 'password'),
-('ジョン', 'password'),
-('サンタクロース', 'password'),
-('先生', 'password');
+('まるお', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('トマト君', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('マスク侍', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('腕時計両腕ちゃん', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('わたしの中の悪魔', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('眠り姫', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('べらぼう太郎', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('ジョン', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('サンタクロース', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta'),
+('先生', '$2a$10$BTZGQmFzueOeJERid6hnTOrINVral3fMrzMl0A4TjlTrBJ/jOW2Ta');
