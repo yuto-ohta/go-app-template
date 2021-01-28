09fab8622627354682c6efb472a21e68a16bcb7a
yuto-ohta <yutohta@gmail.com>
2021-01-29 00:01:25 +0900

    refactor: "name start with package name"を避ける

--- src/usecase/appmodel/session/manager.go
+++ src/usecase/appmodel/session/manager.go
@@ -12,19 +12,19 @@ import (
 
 var _sessionKey = config.GetConfig()["session_key"].(string)
 
-type SessionManager struct {
+type Manager struct {
 }
 
-func NewSessionManager() SessionManager {
-	return SessionManager{}
+func NewSessionManager() Manager {
+	return Manager{}
 }
 
-func (s SessionManager) Get(c echo.Context) *sessions.Session {
+func (s Manager) Get(c echo.Context) *sessions.Session {
 	sess, _ := session.Get(_sessionKey, c)
 	return sess
 }
 
-func (s SessionManager) Set(c echo.Context, key string, value string) error {
+func (s Manager) Set(c echo.Context, key string, value string) error {
 	sess := s.Get(c)
 	sess.Options = &sessions.Options{
 		Path:     "/",
@@ -38,7 +38,7 @@ func (s SessionManager) Set(c echo.Context, key string, value string) error {
 	return nil
 }
 
-func (s SessionManager) Invalidate(c echo.Context) error {
+func (s Manager) Invalidate(c echo.Context) error {
 	sess := s.Get(c)
 	sess.Options.MaxAge = -1
 	if err := sess.Save(c.Request(), c.Response()); err != nil {
